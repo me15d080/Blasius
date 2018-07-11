f7f971b22eae29629fcfd39c3272a1110386914b
Vikas <35677046+me15d080@users.noreply.github.com>
2018-07-11 10:06:12 +0530

    Update usr_input.m

--- usr_input.m
+++ usr_input.m
@@ -7,33 +7,33 @@
 % run the driver_code.m for the numerical solution of boundary layer equations. %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
-%% Geometry
-Lx=10;              %% length of computational domain
-Ly=4;               %% height of computational domain
-Nx=100;             %% No. of x grid points
-Ny=100;             %% No. of y grid points
-%% Flow property
-uinf=35;            %% free stream  velocity
-Tinf=50;            %% free stream temp
-Tplate=30;          %% flat plate surface temperature
-%% Fluid property
-nu=0.8 ;            %% kinematic viscosity 
-k=1;                %% Fluid thermal conductivity
-global Pr;          %% Prandtl Number (k/rho*cp)
+% Geometry
+Lx=10;              %length of computational domain
+Ly=4;               % height of computational domain
+Nx=100;             % No. of x grid points
+Ny=100;             % No. of y grid points
+% Flow property
+uinf=35;            % free stream  velocity
+Tinf=50;            % free stream temp
+Tplate=30;          % flat plate surface temperature
+% Fluid property
+nu=0.8 ;            % kinematic viscosity 
+k=1;                % Fluid thermal conductivity
+global Pr;          % Prandtl Number (k/rho*cp)
 Pr=0.1;
-%% Guesses for Shooting Method %%
+% Guesses for Shooting Method %%
 
-%% User guess for f'' 
+% User guess for f double prime
 
 guess1_f3=0;
 guess2_f3=5;
 
-%% User guess for Theta'
+% User guess for Theta prime
 
 guess1_f5=0;
 guess2_f5=5;
 
-%% Note 
+% Note 
 % f means non dimensional stream function used in Blasius momentum equation.
 % theta means non dimensional temperature used in Pohlhausen Energy equation.
 % f3 mean (d2f/dn2) and f5 means (d2(theta)/d(eta2))
